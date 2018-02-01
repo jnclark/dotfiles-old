20bcd771de4391e04a99d9f8a7111756a8195bc1
jnclark <jnc@jnclark.org>
2018-01-31 20:00:31 -0600

    Began tracking mpd.conf and ncmpcpp config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 dir=$(dirname "$(readlink -f "$0")") # root dotfiles directory
 files="latexmkrc vimrc gitconfig hgrc config/i3/config" # dotfiles to symlink
-folders="vim emacs.d mutt" #dotfolders to symlink
+folders="vim emacs.d mutt mpd ncmpcpp" #dotfolders to symlink
 olddir=$dir/dotfiles_old
 
 
