d6626c0aee51a2c2c86d6aad3a8378b8e0d810a5
jnclark <jnc@jnclark.org>
2018-04-07 18:39:28 -0500

    Removed ox-reveal settings for org

--- emacs.d/init-files/org-config.el
+++ emacs.d/init-files/org-config.el
@@ -1,11 +1,3 @@
-;;org reveal
-
-(require 'ox-reveal)
-
-;;(setq org-reveal-root "../reveal.js")
-(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
-(setq org-reveal-mathjax t)
-
 (setq org-image-actual-width nil)
 
 (require  'htmlize)
