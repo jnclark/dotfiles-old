7d0c99927c4efcff3d41d48d954d41725bd34664
Jacob Clark <jnc@jnclark.org>
2018-04-20 09:57:29 -0500

    Added in zathurarc with synctex and highlighting

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 dir=$(dirname "$(readlink -f "$0")") # root dotfiles directory
-files="profile bash_aliases latexmkrc vimrc gitconfig hgrc config/i3/config" # dotfiles to symlink
+files="profile bash_aliases latexmkrc vimrc gitconfig hgrc config/i3/config config/zathura/zathurarc" # dotfiles to symlink
 folders="vim emacs.d mutt mpd ncmpcpp" #dotfolders to symlink
 olddir=$dir/dotfiles_old
 
